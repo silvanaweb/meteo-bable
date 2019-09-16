38c4bc258d1bfd52102fc0ebe676576768692669
silvanaweb <silvanasono@yahoo.it>
2019-09-16 16:37:42 +0200

    Set up local  environment variables

--- src/data/weather.data.ts
+++ src/data/weather.data.ts
@@ -1,5 +1,11 @@
 // This file provide the weather info. It fetches them from an API call.
 
+let API_KEY: unknown = process.env.REACT_APP_API_KEY;
+let REACT_APP_API_URL: unknown = process.env.REACT_APP_API_URL;
+
+console.log(API_KEY);
+console.log(REACT_APP_API_URL);
+
 export type WeatherInfo = {
   location: string;
   summary: string;
