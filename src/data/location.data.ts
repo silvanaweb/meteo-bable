600cced07138318fac0b4d17233a075af4ff21fd
silvanaweb <silvanasono@yahoo.it>
2019-09-16 10:21:59 +0200

    Create location and weather mock data

---
+++ src/data/location.data.ts
@@ -0,0 +1,44 @@
+// This file provide the location DataCue, that means city and position.
+// If the cities were a user input, we would have used an api call to a coordinate locator.
+// Because we know the cities, we can hardcode their coordinates in a dictionary.
+// We also use union types as helpers
+
+export type Places = "london" | "paris" | "newyork" | "singapore" | "sydney";
+export type Location = {
+  name: string;
+  latitude: number;
+  longitude: number;
+};
+export type LocationsMap = { [key in Places]: Location };
+
+const locations: LocationsMap = {
+  london: {
+    name: "London",
+    latitude: 51.507351,
+    longitude: -0.127758
+  },
+  paris: {
+    name: "Paris",
+    latitude: 48.856613,
+    longitude: 2.352222
+  },
+  newyork: {
+    name: "New York",
+    latitude: 40.712776,
+    longitude: -74.005974
+  },
+  singapore: {
+    name: "Singapore",
+    latitude: 1.28944,
+    longitude: 103.849983
+  },
+  sydney: {
+    name: "Sydney",
+    latitude: -33.86882,
+    longitude: 151.20929
+  }
+};
+
+export function getLocations() {
+  return { ...locations };
+}
---
+++ src/data/weather.data.ts
@@ -0,0 +1,25 @@
+// This file provide the weather info. It fetches them from an API call.
+
+export type WeatherInfo = {
+  location: string;
+  summary: string;
+  temperature: number;
+  icon: string;
+};
+
+export function getWeatherFromLocation(
+  latitude: number,
+  longitude: number
+): Promise<WeatherInfo> {
+  // simulate API call
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({
+        location: "London",
+        summary: "Rainy days",
+        temperature: 20,
+        icon: "rain"
+      });
+    }, 2000);
+  });
+}
